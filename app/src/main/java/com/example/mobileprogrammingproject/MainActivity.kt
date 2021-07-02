7dd22fdc0ed886922a9e90c25816531cb7872696
Federico <federico.cappellini99@gmail.com>
2021-07-02 16:37:07 +0200

    Aggiunta del sistema di binding per i pulsanti

--- app/src/main/java/com/example/mobileprogrammingproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogrammingproject/MainActivity.kt
@@ -3,13 +3,16 @@ package com.example.mobileprogrammingproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.example.mobileprogrammingproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val MyButtonRules = findViewById<Button>(R.id.Rules_Button)
-        MyButtonRules.setOnClickListener{
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.RulesButton.setOnClickListener{
             val Rules_file = RuleDialog()
             Rules_file.show(supportFragmentManager, "001")
         }
