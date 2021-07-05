5d67a60b6a5ad543ea348a7977e048c7720470e5
Federico <federico.cappellini99@gmail.com>
2021-07-05 12:14:02 +0200

    Aggiunta dell'ExitGameDialog per gestire il back button di PlayActivity

---
+++ app/src/main/java/com/example/mobileprogrammingproject/ExitGameDialog.kt
@@ -0,0 +1,21 @@
+package com.example.mobileprogrammingproject
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class ExitGameDialog : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setMessage(R.string.leaving)
+                .setPositiveButton(R.string.yes) { _: DialogInterface, _: Int ->
+                    it.finish()
+                }
+            builder.setNegativeButton(R.string.no) { _: DialogInterface, _: Int -> }
+            builder.create()
+        } ?: throw IllegalStateException("Error")
+    }
+}
