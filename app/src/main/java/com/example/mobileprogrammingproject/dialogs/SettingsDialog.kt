fdbf4eed7174f27e3a4aba41bf02aab7ed723e1b
Federico <federico.cappellini99@gmail.com>
2021-07-08 02:32:32 +0200

    vatte a vede er todo comunque avemo mezzo risolto er dialog

--- app/src/main/java/com/example/mobileprogrammingproject/dialogs/SettingsDialog.kt
+++ app/src/main/java/com/example/mobileprogrammingproject/dialogs/SettingsDialog.kt
@@ -4,23 +4,28 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.graphics.component1
 import androidx.fragment.app.DialogFragment
 import com.example.mobileprogrammingproject.R
 import java.lang.Exception
 import java.lang.IllegalStateException
+import kotlin.properties.Delegates
 
 class SettingsDialog : DialogFragment() {
+    private var MatchType by Delegates.notNull<Int>()
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
-            var MatchType : Int
+            var position = 0
+            val list: Array<out String> = it.resources.getStringArray(R.array.SelectMatch)
             val builder = AlertDialog.Builder(it)
             builder.setTitle("Choose the number of players that will play the game...")
-            builder.setSingleChoiceItems(R.array.SelectMatch,0) { _: DialogInterface, _: Int -> }
-                .setNegativeButton(R.string.Cancel) { dialogInterface: DialogInterface, i: Int ->
-                    it.finish()
+            builder.setItems(list) { _: DialogInterface, i: Int ->
+                //todo va sistemato
+                if(list[i] == "Singleplayer(training)"){
+                    Toast.makeText(it, "bravo", Toast.LENGTH_SHORT).show()
                 }
-                .setPositiveButton(R.string.Ok) { _: DialogInterface, _: Int ->
-                }
+            }
             builder.create()
         }?:throw IllegalStateException("ripijate")
 
