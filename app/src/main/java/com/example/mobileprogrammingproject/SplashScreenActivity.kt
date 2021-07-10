fd5cb6aca38fceb57cc6b18b8b8fdce37a43ad77
Giordano <giordano.spirito@students.uniroma2.eu>
2021-07-10 11:57:57 +0200

    no night mode

--- app/src/main/java/com/example/mobileprogrammingproject/SplashScreenActivity.kt
+++ app/src/main/java/com/example/mobileprogrammingproject/SplashScreenActivity.kt
@@ -3,10 +3,12 @@ package com.example.mobileprogrammingproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         startActivity(Intent(applicationContext, MainMenuActivity::class.java))
         finish()
     }
